df4110dbb070fcbfe5d15ebc36c21812cbf6ec50
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-05-09 16:14:59 +0500

    two project done

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,121 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Павел Кай on 09.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US", "Monaco"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var score = 0
+    
+    @State private var times = 0
+    @State private var showingFinal = false
+    @State private var finalTitle = ""
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    var body: some View {
+        ZStack {
+            
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert(finalTitle, isPresented: $showingFinal) {
+            Button("Reset game") {
+                reset()
+            }
+        } message: {
+            Text("Final score is \(score)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That's flag of \(countries[number])"
+            score -= 1
+        }
+        
+        times += 1
+        if times == 8 {
+            showingFinal = true
+        } else {
+            showingScore = true
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        times = 0
+        score = 0
+        askQuestion()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Павел Кай on 26.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = tipValue + checkAmount
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let total = checkAmount + tipValue
+        
+        return total
+    }
+    
+    var currency: FloatingPointFormatStyle<Double>.Currency {
+        return .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currency)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                } header: {
+                    Text("How much tip you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currency)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section {
+                    Text(totalAmount, format: currency)
+                } header: {
+                    Text("Total Amount")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
