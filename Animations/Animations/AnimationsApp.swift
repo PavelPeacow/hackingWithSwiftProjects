8371a612dccecbba455585fea18ced20a3f7b5d4
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-05-13 22:52:18 +0500

    project Animations is done

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Павел Кай on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
