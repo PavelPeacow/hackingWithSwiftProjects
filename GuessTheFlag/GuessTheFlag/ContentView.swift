fd5e8bd86ed77c8ac4efdae721513f635fdbe2c8
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-05-14 23:17:05 +0500

    Added animations

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct FlagImage: View {
     var numberImage: Int
     var arrayImage: [String]
+    var correctAnimation: Bool
+    var wrongAnimation: Bool
+    var correctAnswer: Int
+    var numberOfFlag: Int
     
     var body: some View {
         Image(arrayImage[numberImage])
             .renderingMode(.original)
             .clipShape(Capsule())
+            .overlay(
+                Capsule()
+                    .stroke(correctAnimation && correctAnswer == numberOfFlag
+                            ? .green : .black, lineWidth: 5)
+            )
             .shadow(radius: 5)
+            .rotation3DEffect(
+                .degrees(correctAnimation && correctAnswer == numberOfFlag ? 360 : 0),
+                axis: (x: 0, y: 1, z: 0))
+            .opacity(correctAnimation && correctAnswer != numberOfFlag ? 0.25 : 1)
+            .opacity(wrongAnimation && correctAnswer != numberOfFlag ? 0.25 : 1)
+            .scaleEffect(correctAnimation && correctAnswer != numberOfFlag ? 0.7 : 1)
+            .scaleEffect(wrongAnimation && correctAnswer != numberOfFlag ? 0.7 : 1)
+            
     }
 }
 
@@ -32,6 +49,9 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var correctAnimation = false
+    @State private var wrongAnimation = false
+    
     var body: some View {
         ZStack {
             
@@ -39,7 +59,7 @@ struct ContentView: View {
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack {
                 Spacer()
@@ -67,7 +87,12 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            FlagImage(numberImage: number, arrayImage: countries)
+                            FlagImage(numberImage: number,
+                                      arrayImage: countries,
+                                      correctAnimation: correctAnimation,
+                                      wrongAnimation: wrongAnimation,
+                                      correctAnswer: correctAnswer,
+                                      numberOfFlag: number)
                         }
                     }
                     
@@ -75,17 +100,33 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
             .padding()
         }
+        
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
+            Button("Continue") {
+                if correctAnimation {
+                    correctAnimation.toggle()
+                }
+                if wrongAnimation {
+                    wrongAnimation.toggle()
+                }
+                askQuestion()
+            }
         } message: {
             Text("Your score is \(score)")
         }
+        
         .alert(finalTitle, isPresented: $showingFinal) {
             Button("Reset game") {
+                if correctAnimation {
+                    correctAnimation.toggle()
+                }
+                if wrongAnimation {
+                    wrongAnimation.toggle()
+                }
                 reset()
             }
         } message: {
@@ -97,9 +138,17 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            
+            withAnimation {
+                correctAnimation.toggle()
+            }
         } else {
             scoreTitle = "Wrong! That's flag of \(countries[number])"
             score -= 1
+            
+            withAnimation {
+                wrongAnimation.toggle()
+            }
         }
         
         times += 1
